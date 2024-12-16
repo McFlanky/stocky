a1946d598d679174d48ee18708fa71514759b891
McFlanky <cturjanica02@gmail.com>
2024-12-16 15:44:22 -0500

    Added portfolio GET method by adding a claims extension class to get user claims and adding Portfolio repo/interface to integrate it into a Portfolio controller to get a users entire portfolio by using the username from the JWT claims from TokenService to grab their username from the DB context to then get user portfolio from port repo by using the app user find name by async function

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ff3d09e270010d8d89b2ba91ec2797b69d9afa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5a1989840cabf8a7c780d61f49aafe253e378b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
