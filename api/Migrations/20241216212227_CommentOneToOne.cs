089b404e9ec3d719efbe606684762b7866269358
McFlanky <cturjanica02@gmail.com>
2024-12-16 16:38:19 -0500

    Added a way to view user generated content (comments and username) by adding createdBy to comment Dto and adding the appuser Id to commentModel before its created and to include LINQ include method in Comment Repo Get all and by Id async methods, as well as include comments in stock in stock repo in getAllAsync method with LINQ include/thenInclude methods and ToCommentDto has CreatedBy field with AppUser.UserName

---
+++ api/Migrations/20241216212227_CommentOneToOne.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentOneToOne : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e7228261-a9b7-4a7e-8843-631f7f9f26bc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e7f5eb9d-0b10-42a6-b9e6-3348f0b3990e");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppUserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "354d7d43-4fcc-4a5e-83a6-db270d6fd188", null, "User", "USER" },
+                    { "74fa4b06-7656-494b-ac56-71ad55cfb950", null, "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_AppUserId",
+                table: "Comments");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "354d7d43-4fcc-4a5e-83a6-db270d6fd188");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "74fa4b06-7656-494b-ac56-71ad55cfb950");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Comments");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "e7228261-a9b7-4a7e-8843-631f7f9f26bc", null, "Admin", "ADMIN" },
+                    { "e7f5eb9d-0b10-42a6-b9e6-3348f0b3990e", null, "User", "USER" }
+                });
+        }
+    }
+}
